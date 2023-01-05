059df9e73c141536c1a3f67f3759e77282c6c30f
Daniil <95318722+Fallen-Eleonor@users.noreply.github.com>
2023-01-05 09:45:19 -0800

    Finally

---
+++ src/mx_print_strarr.c
@@ -0,0 +1,15 @@
+#include "../inc/libmx.h"
+
+void mx_print_strarr(char **arr, const char *delim)
+{
+    if (arr == NULL || delim == NULL) {
+        return;
+    }
+    for (int i = 0; arr != NULL; i++) {
+       mx_printstr(*arr++);
+       mx_printchar(*delim);
+    }
+}
+
+
+
