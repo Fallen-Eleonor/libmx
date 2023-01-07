04005d7aa3b46929a749cb877644b710be4efca6
Daniil <95318722+Fallen-Eleonor@users.noreply.github.com>
2023-01-06 19:37:13 -0800

    Add files via upload

--- src/mx_print_strarr.c
+++ src/mx_print_strarr.c
@@ -2,14 +2,15 @@
 
 void mx_print_strarr(char **arr, const char *delim)
 {
-    if (arr == NULL || delim == NULL) {
+    if (arr == NULL || delim == NULL)
+    {
         return;
     }
-    for (int i = 0; arr != NULL; i++) {
-       mx_printstr(*arr++);
-       mx_printchar(char *delim);
+
+    for (int i = 0; arr != NULL; i++)
+    {
+       mx_printstr(*(arr++));
+       mx_printchar((char)delim);
     }
 }
-
-
 
